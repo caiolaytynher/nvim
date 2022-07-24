0d2e77cf63cc62666121ce22260397169cb1d378
caiolaytynher <caiolaytynherbr@gmail.com>
2022-07-24 04:34:54 -0300

    changed tabs to spaces

--- lua/caio/colorscheme.lua
+++ lua/caio/colorscheme.lua
@@ -1,13 +1,13 @@
 vim.cmd [[
-    if exists("&termguicolors") && exists("&winblend")
-        syntax enable
-        set termguicolors
-        set winblend=0
-        set wildoptions=pum
-        set pumblend=5
-        set background=dark
-        colorscheme gruvbox
-        highlight Normal guibg=None
-        highlight NonText guibg=None
-    endif
+  if exists("&termguicolors") && exists("&winblend")
+      syntax enable
+      set termguicolors
+      set winblend=0
+      set wildoptions=pum
+      set pumblend=5
+      set background=dark
+      colorscheme gruvbox
+      highlight Normal guibg=None
+      highlight NonText guibg=None
+  endif
 ]]
