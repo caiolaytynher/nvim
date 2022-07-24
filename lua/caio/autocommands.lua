0dd9a9d27b57fdc09ed69b98c28cb19a97c0dd2c
caiolaytynher <caiolaytynherbr@gmail.com>
2022-07-24 20:16:39 -0300

    added autogroups

--- lua/caio/autocommands.lua
+++ lua/caio/autocommands.lua
@@ -1,21 +1,30 @@
-vim.api.nvim_create_autocmd(
-  "FileType",
-  {
-    pattern = "python",
-    callback = function()
-      vim.opt.shiftwidth = 4
-      vim.opt.tabstop = 4
-      vim.opt.softtabstop = 4
-    end,
-  }
-)
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
 
-vim.api.nvim_create_autocmd(
-  "FileType",
-  {
-    pattern = { "tex", "markdown" },
-    callback = function()
-      vim.opt.tw = 67
-    end,
-  }
-)
+local file_specific_group = augroup("FileSpecific", {})
+local custom_fn_group = augroup("CustomFn", {})
+
+autocmd("FileType", {
+  group = file_specific_group,
+  pattern = "python",
+  callback = function()
+    vim.opt.shiftwidth = 4
+    vim.opt.tabstop = 4
+    vim.opt.softtabstop = 4
+  end,
+})
+
+autocmd("FileType", {
+  group = file_specific_group,
+  pattern = { "tex", "markdown" },
+  callback = function()
+    vim.opt.tw = 67
+  end,
+})
+
+-- Trim whitespaces
+autocmd("BufWritePre", {
+    group = custom_fn_group,
+    pattern = "*",
+    command = "%s/\\s\\+$//e",
+})
