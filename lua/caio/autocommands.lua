2490ea0e5a438e4fa539b0969d13c597d73a94c6
caiolaytynher <caiolaytynherbr@gmail.com>
2022-09-20 16:38:47 -0300

    changed behavior of tex and md files

--- lua/caio/autocommands.lua
+++ lua/caio/autocommands.lua
@@ -5,34 +5,35 @@ local file_specific_group = augroup("FileSpecific", { clear = true })
 local custom_fn_group = augroup("CustomFn", { clear = true })
 
 autocmd("FileType", {
-  group = file_specific_group,
-  pattern = "python",
-  callback = function()
-    vim.opt_local.shiftwidth = 4
-    vim.opt_local.tabstop = 4
-    vim.opt_local.softtabstop = 4
-  end,
+	group = file_specific_group,
+	pattern = "python",
+	callback = function()
+		vim.opt_local.shiftwidth = 4
+		vim.opt_local.tabstop = 4
+		vim.opt_local.softtabstop = 4
+	end,
 })
 
 autocmd("FileType", {
-  group = file_specific_group,
-  pattern = { "tex", "markdown" },
-  callback = function()
-    vim.opt_local.tw = 67
-  end,
+	group = file_specific_group,
+	pattern = { "tex", "markdown" },
+	callback = function()
+		vim.opt_local.tw = 65
+		vim.opt_local.sidescrolloff = 0
+	end,
 })
 
 -- Trim whitespaces
 autocmd("BufWritePre", {
-  group = custom_fn_group,
-  pattern = "*",
-  command = "%s/\\s\\+$//e",
+	group = custom_fn_group,
+	pattern = "*",
+	command = "%s/\\s\\+$//e",
 })
 
 autocmd("BufWritePre", {
-  group = custom_fn_group,
-  pattern = "*",
-  callback = function()
-    vim.lsp.buf.formatting_sync()
-  end,
+	group = custom_fn_group,
+	pattern = "*",
+	callback = function()
+		vim.lsp.buf.formatting_sync()
+	end,
 })
