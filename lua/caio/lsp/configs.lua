fb5f6ee7b7edc00fc2383744d37a2f6f09b1d2df
caiolaytynher <caiolaytynherbr@gmail.com>
2022-09-20 13:39:26 -0300

    added c lsp

--- lua/caio/lsp/configs.lua
+++ lua/caio/lsp/configs.lua
@@ -1,24 +1,26 @@
 local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
-if not status_ok then return end
+if not status_ok then
+	return
+end
 
 local lspconfig = require("lspconfig")
 
-local servers = { "sumneko_lua", "pyright" }
+local servers = { "sumneko_lua", "pyright", "clangd" }
 
 lsp_installer.setup({
-  ensure_installed = servers,
+	ensure_installed = servers,
 })
 
 for _, server in pairs(servers) do
-  local opts = {
-    on_attach = require("caio.lsp.handlers").on_attach,
-    capabilities = require("caio.lsp.handlers").capabilities,
-  }
+	local opts = {
+		on_attach = require("caio.lsp.handlers").on_attach,
+		capabilities = require("caio.lsp.handlers").capabilities,
+	}
 
-  local has_custom_opts, server_custom_opts = pcall(require, "caio.lsp.settings." .. server)
-  if has_custom_opts then
-    opts = vim.tbl_deep_extend("force", opts, server_custom_opts)
-  end
+	local has_custom_opts, server_custom_opts = pcall(require, "caio.lsp.settings." .. server)
+	if has_custom_opts then
+		opts = vim.tbl_deep_extend("force", opts, server_custom_opts)
+	end
 
-  lspconfig[server].setup(opts)
+	lspconfig[server].setup(opts)
 end
