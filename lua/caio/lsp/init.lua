a8e2165a57c5ef6253d98fe36c39e6be4c67af9d
caiolaytynher <caiolaytynherbr@gmail.com>
2022-12-26 20:41:14 -0300

    added helper links for new lsp configs

--- lua/caio/lsp/init.lua
+++ lua/caio/lsp/init.lua
@@ -1,6 +1,10 @@
 local status_ok, _ = pcall(require, "lspconfig")
-if not status_ok then return end
+if not status_ok then
+	return
+end
 
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+-- https://github.com/neovim/nvim-lspconfig/tree/master/lua/lspconfig/server_configurations
 require("caio.lsp.configs")
 require("caio.lsp.handlers").setup()
 require("caio.lsp.null-ls")
